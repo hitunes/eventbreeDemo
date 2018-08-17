2e061af391f07cbcee24d7a4e487536876b32f39
Itunu Adekoya <hitunes247@gmail.com>
2018-08-17 11:10:46 +0100

    fix select hover issue

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Input, Icon, Menu, Dropdown } from "antd";
+import { Events, Culture, Category } from "../../helpers.js";
 import "./Search.css";
 
 const Search = Input.Search;
@@ -30,84 +31,9 @@ export default class SearchInput extends Component {
     );
   }
 }
-let culture = [
-  "Asian",
-  "African",
-  "European",
-  "Indian",
-  "Western",
-  "Jewish",
-  "Arabian",
-  "Greek",
-  "Italian",
-  "English",
-  "General",
-  "Other"
-];
-let Events = [
-  "Bachelorette Party",
-  "Bachelor's Eve",
-  "Funerals",
-  "Kids Party",
-  "Birthdays",
-  "Graduations",
-  "Reunions",
-  "Christening / Naming",
-  "Ceremonies",
-  "Anniversaries",
-  "Social events",
-  "Baby showers",
-  "Weddings"
-];
-let Category = [
-  "Floral",
-  "Cakes",
-  " Canape",
-  "Event Website",
-  "Kids Entertainment",
-  "Fabrics",
-  "Stationaries",
-  "Transportation",
-  "Venue",
-  "Mcs & Toastmaster",
-  "Wigs & Weaves",
-  "Jewellery",
-  "Gift Service",
-  "Cakes & Desserts",
-  "Drinks & Cocktails",
-  "Music Entertaiment",
-  "Traditional Wedding Gifts",
-  "Lighting & Sounds",
-  "Desserts",
-  "Party Host",
-  "Dancing Entertainment",
-  "Storyteller",
-  "Decoration",
-  "Honeymoon",
-  "Fashion Accessories",
-  "Invitation Cards",
-  "Event & Party Rentals",
-  "Catering Services",
-  "Balloon Artstery",
-  "Photography",
-  "Balloon Twisters",
-  "Bands",
-  "Bridal Wears",
-  "Mixologist",
-  "Videography",
-  "Fashion & Style",
-  "Media Coverage",
-  "Wedding Decorator",
-  "Menswear",
-  "Hosts & Hostesses",
-  "Event Security",
-  "Waiters & Waitresses",
-  "Beauty, Hair & Make-up",
-  "Kids Play Center",
-  "Djs",
-  "Event Planners",
-  "Party Entertainment"
-];
+// const onClick = function({ key }) {
+//   message.info(`Click on item ${key}`);
+// };
 const menu = (
   <Menu>
     <SubMenu title="Events">
@@ -121,7 +47,7 @@ const menu = (
       ))}
     </SubMenu>
     <SubMenu title="Culture">
-      {culture.map((value, index) => (
+      {Culture.map((value, index) => (
         <Menu.Item key={index}>{value}</Menu.Item>
       ))}
     </SubMenu>
@@ -129,10 +55,10 @@ const menu = (
 );
 const TrendsDropdown = params => {
   return (
-    <Dropdown overlay={menu}>
-      <span className="ant-dropdown-link">
+    <Dropdown overlay={menu} trigger={["click"]}>
+      <a className="ant-dropdown-link" href="#">
         Trends <Icon type="down" />
-      </span>
+      </a>
     </Dropdown>
   );
 };
